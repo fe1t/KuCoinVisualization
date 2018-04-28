1a758d35f5f906d29a57610815d832d021e6ca4e
Nuttapon Thanitsukkan <nuttapon.t@ku.th>
2018-04-29 05:45:22 +0700

    base structure & wait for raw

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
-import React, {
-  Component
-} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { Component } from 'react'
+import Blockchain from './containers/visualize'
+import logo from './logo.svg'
+import Api from './common/api/manager'
+import Immutable from 'immutable'
+
+const raw = {
+  name: 'Hello World',
+  children: [{ name: 'pispa pispa', children: null }]
+}
+
+const fetchData = async url => {
+  const blocks = (await Api.get(url)).data
+  return blocks
+}
+
+function MyBlockchain(props) {
+  let data = fetchData(props.url)
+  return data.then(d => <Blockchain width="1435" height="800" raw={d} />)
+}
 
 class App extends Component {
   render() {
-    return ( <
-      div className = "App" >
-      <
-      header className = "App-header" >
-      <
-      img src = {
-        logo
-      }
-      className = "App-logo"
-      alt = "logo" / >
-      <
-      h1 className = "App-title" > Welcome to React < /h1> < /
-      header > <
-      p className = "App-intro" >
-      To get started, edit < code > src / App.js < /code> and save to reload. < /
-      p > <
-      /div>
-    );
+    return (
+      <div className="App">
+        <MyBlockchain url="http://localhost:23000/blocks" />
+      </div>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/containers/visualize.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import { Group } from '@vx/group'
+import { Tree } from '@vx/hierarchy'
+import { LinkHorizontal } from '@vx/shape'
+import { hierarchy } from 'd3-hierarchy'
+import { LinearGradient } from '@vx/gradient'
+
+function Node({ node, events }) {
+  const width = 40
+  const height = 20
+  return (
+    <Group top={node.x} left={node.y}>
+      {node.depth === 0 && <circle r={12} fill="url('#lg')" />}
+      {node.depth !== 0 && (
+        <rect
+          height={height}
+          width={width}
+          y={-height / 2}
+          x={-width / 2}
+          fill={'#272b4d'}
+          stroke={node.children ? '#03c0dc' : '#26deb0'}
+          strokeWidth={1}
+          strokeDasharray={!node.children ? '2,2' : '0'}
+          strokeOpacity={!node.children ? 0.6 : 1}
+          rx={!node.children ? 10 : 0}
+          onClick={() => {
+            alert(`clicked: ${JSON.stringify(node.data.name)}`)
+          }}
+        />
+      )}
+      <text
+        dy={'.33em'}
+        fontSize={9}
+        fontFamily="Arial"
+        textAnchor={'middle'}
+        style={{ pointerEvents: 'none' }}
+        fill={node.depth === 0 ? '#71248e' : node.children ? 'white' : '#26deb0'}
+      >
+        {node.data.name}
+      </text>
+    </Group>
+  )
+}
+
+function Link({ link }) {
+  return <LinkHorizontal data={link} stroke="#374469" strokeWidth="1" fill="none" />
+}
+
+export default ({
+  width,
+  height,
+  raw,
+  events = false,
+  margin = {
+    top: 10,
+    left: 30,
+    right: 40,
+    bottom: 80
+  }
+}) => {
+  const data = hierarchy(raw)
+  return (
+    <svg width={width} height={height}>
+      <LinearGradient id="lg" from="#fd9b93" to="#fe6e9e" />
+      <rect width={width} height={height} rx={14} fill="#272b4d" />
+      <Tree
+        top={margin.top}
+        left={margin.left}
+        root={data}
+        size={[height - margin.top - margin.bottom, width - margin.left - margin.right]}
+        nodeComponent={Node}
+        linkComponent={Link}
+      />
+    </svg>
+  )
+}
